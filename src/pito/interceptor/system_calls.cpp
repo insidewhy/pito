31975e41790639e40136a7f5ea7a297ff2d7f0fa
jpike <jpike@afterclap.com>
2009-01-18 20:36:38 +0000

    libc.h forgotten commit
    git-svn-id: svn://afterclap.com/pito/trunk@15 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/interceptor/system_calls.cpp
+++ src/pito/interceptor/system_calls.cpp
@@ -4,64 +4,6 @@
 
 extern "C" {
 
-////////////////////////////////////////////////////////////////////////////////
-// security intercepts
-// function todo: chmod
-// function todo: fchmodat
-// function todo: chown
-// function todo: fchownat
-
-// dunno aobut this
-// int open(const char *pathname, int flags) {
-// }
-
-int open(const char *pathname, int flags, mode_t mode) {
-}
-
-// function todo: openat
-
-int creat(const char *pathname, mode_t mode) {
-}
-
-// function todo: fopen
-// function todo: lchown
-// function todo: link
-// function todo: linkat
-// function todo: mkdir
-// function todo: mkdirat
-// function todo: opendir
-// function todo: mknod
-// function todo: mknodat
-// function todo: __xmknod
-// function todo: mkfifo
-// function todo: mkfifoat
-// function todo: access
-// function todo: faccessat
-// function todo: rename
-// function todo: renameat
-// function todo: rmdir
-// function todo: symlink
-// function todo: symlinkat
-// function todo: truncate
-// function todo: unlink
-// function todo: unlinkat
-// function todo: getcwd
-// function todo: open64
-// function todo: openat64
-// function todo: creat64
-// function todo: fopen64
-// function todo: truncate64
-// function todo: execve
-// function todo: utime
-// function todo: utimes
-// function todo: utimensat
-// function todo: futimesat
-// function todo: lutimes
-
-////////////////////////////////////////////////////////////////////////////////
-// extra intercepts
-uid_t getuid(void) {
-    return 0;
-}
+// #include "SystemCallsGenerated.hpp"
 
 }
