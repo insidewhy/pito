0430817c83ddd0473a2600bd5a141d719a1599f0
jpike <jpike@afterclap.com>
2009-01-18 22:38:31 +0000

    libc overrides
    git-svn-id: svn://afterclap.com/pito/trunk@17 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/interceptor/libc.h
+++ src/pito/interceptor/libc.h
@@ -1,56 +1,47 @@
 ////////////////////////////////////////////////////////////////////////////////
 // security intercepts
-// function todo: chmod
-// function todo: fchmodat
-// function todo: chown
-// function todo: fchownat
-
+int chmod(const char *path, mode_t mode);
+int fchmod(int fd, mode_t mode);
+int fchmodat(int dirfd, const char *pathname, mode_t mode, int flags);
+int chown(const char *path, uid_t owner, gid_t group);
+int fchown(int fd, uid_t owner, gid_t group);
+int fchownat(int dirfd, const char *pathname, uid_t owner, gid_t group, int flags);
 // see what to do about optional argument
-int open(const char *pathname, int flags, mode_t mode) {
-}
-
-// function todo: openat
-int creat(const char *pathname, mode_t mode) {
-}
-
-// function todo: fopen
-// function todo: lchown
-// function todo: link
-// function todo: linkat
-// function todo: mkdir
-// function todo: mkdirat
-// function todo: opendir
-// function todo: mknod
-// function todo: mknodat
+int open(const char *pathname, int flags, mode_t mode);
+int openat(int dirfd, const char *pathname, int flags, mode_t mode);
+int creat(const char *pathname, mode_t mode);
+FILE *fopen(const char *path, const char *mode);
+int lchown(const char *path, uid_t owner, gid_t group);
+int link(const char *oldpath, const char *newpath);
+int linkat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, int flags);
+int mkdir(const char *pathname, mode_t mode);
+int mkdirat(int dirfd, const char *pathname, mode_t mode);
+DIR *opendir(const char *name);
+int mknod(const char *pathname, mode_t mode, dev_t dev);
+int mknodat(int dirfd, const char *pathname, mode_t mode, dev_t dev);
 // function todo: __xmknod
-// function todo: mkfifo
-// function todo: mkfifoat
-// function todo: access
-// function todo: faccessat
-// function todo: rename
-// function todo: renameat
-// function todo: rmdir
-// function todo: symlink
-// function todo: symlinkat
-// function todo: truncate
-// function todo: unlink
-// function todo: unlinkat
-// function todo: getcwd
-// function todo: open64
-// function todo: openat64
-// function todo: creat64
-// function todo: fopen64
-// function todo: truncate64
-// function todo: execve
-// function todo: utime
-// function todo: utimes
-// function todo: utimensat
-// function todo: futimesat
-// function todo: lutimes
-
-////////////////////////////////////////////////////////////////////////////////
-// extra intercepts
-uid_t getuid(void) {
-    return 0;
-}
-
+int mkfifo(const char *pathname, mode_t mode);
+int mkfifoat(int dirfd, const char *pathname, mode_t mode);
+int access(const char *pathname, int mode);
+int faccessat(int dirfd, const char *pathname, int mode, int flags);
+int rename(const char *oldpath, const char *newpath);
+int renameat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath);
+int rmdir(const char *pathname);
+int symlink(const char *oldpath, const char *newpath);
+int symlinkat(const char *oldpath, int newdirfd, const char *newpath);
+int truncate(const char *path, off_t length);
+int unlink(const char *pathname);
+int unlinkat(int dirfd, const char *pathname, int flags);
+char *getcwd(char *buf, size_t size);
+int open64(const char *pathname, int flags, mode_t mode);
+int openat64(int dirfd, const char *pathname, int flags, mode_t mode);
+int creat64(const char *pathname, mode_t mode);
+FILE *fopen64(const char *path, const char *mode);
+int truncate64(const char *path, off_t length);
+int execve(const char *filename, char *const argv[], char *const envp[]);
+int utime(const char *filename, const struct utimbuf *times);
+int utimes(const char *filename, const struct timeval times[2]);
+int utimensat(int dirfd, const char *pathname, const struct timespec times[2], int flags);
+int futimesat(int dirfd, const char *pathname, const struct timeval times[2]); 
+int lutimes(const char *filename, const struct timeval tv[2]);
+uid_t getuid(void);
