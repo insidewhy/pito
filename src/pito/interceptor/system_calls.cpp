44c1a698ad92628da55183c5a818734b07139a22
jpike <jpike@afterclap.com>
2009-01-20 19:39:37 +0000

    oh yeah
    git-svn-id: svn://afterclap.com/pito/trunk@25 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/interceptor/system_calls.cpp
+++ src/pito/interceptor/system_calls.cpp
@@ -3,15 +3,16 @@
 extern "C" {
 
 using namespace pito::interceptor;
+using system_call::instance;
 
 // #include "SystemCallsGenerated.hpp"
 
 int open(const char *pathname, int flags, mode_t mode) {
-    return system_call::instance<system_call::open>()(pathname, flags, mode);
+    return instance<system_call::open>()(pathname, flags, mode);
 }
 
 int open64(const char *pathname, int flags, mode_t mode) {
-    return system_call::instance<system_call::open>()(pathname, flags, mode);
+    return instance<system_call::open>()(pathname, flags, mode);
 }
 
 }
