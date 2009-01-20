5070ead03e42c79ae632269f21cc96b297714e41
jpike <jpike@afterclap.com>
2009-01-20 20:01:57 +0000

    make it even easier to wrap calls
    git-svn-id: svn://afterclap.com/pito/trunk@26 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/interceptor/system_calls.cpp
+++ src/pito/interceptor/system_calls.cpp
@@ -1,18 +1,20 @@
 #include <pito/interceptor/SystemCall.hpp>
+#include <iostream>
 
 extern "C" {
 
 using namespace pito::interceptor;
-using system_call::instance;
 
-// #include "SystemCallsGenerated.hpp"
+#define super(calltype)   system_call::instance<system_call:: calltype >()
 
 int open(const char *pathname, int flags, mode_t mode) {
-    return instance<system_call::open>()(pathname, flags, mode);
+    std::cout << "wrapping open(" << pathname << ", " << flags << ", " << mode << ")\n";
+    return super(open)(pathname, flags, mode);
 }
 
 int open64(const char *pathname, int flags, mode_t mode) {
-    return instance<system_call::open>()(pathname, flags, mode);
+    std::cout << "wrapping open64(" << pathname << ", " << flags << ", " << mode << ")\n";
+    return super(open)(pathname, flags, mode);
 }
 
 }
