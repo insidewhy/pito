22c61b343bf6438f75a2641b8a09ef0e488446d0
jpike <jpike@afterclap.com>
2009-01-21 03:49:32 +0000

    gonna handle the names differently
    git-svn-id: svn://afterclap.com/pito/trunk@49 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/interceptor/lib/c.hpp
+++ src/pito/interceptor/lib/c.hpp
@@ -17,52 +17,60 @@ struct Library<library::c> : LibraryHelper {
 };
 
 namespace system_call {
-    struct chmod {};
-    struct fchmod {};
-    struct fchmodat {};
-    struct chown {};
-    struct fchown {};
-    struct fchownat {};
-    struct open {};
-    struct openat {};
-    struct creat {};
-    struct fopen {};
-    struct lchown {};
-    struct link {};
-    struct linkat {};
-    struct mkdir {};
-    struct mkdirat {};
-    struct opendir {};
-    struct mknod {};
-    struct mknodat {};
+
+#define PITO_SYSTEM_CALL_TRAIT(name_) \
+    struct name_ { \
+        static char const name[]; \
+    }; \
+    char const name_::name[] = "name_";
+
+PITO_SYSTEM_CALL_TRAIT(chmod)
+PITO_SYSTEM_CALL_TRAIT(fchmod)
+PITO_SYSTEM_CALL_TRAIT(fchmodat)
+PITO_SYSTEM_CALL_TRAIT(chown)
+PITO_SYSTEM_CALL_TRAIT(fchown)
+PITO_SYSTEM_CALL_TRAIT(fchownat)
+PITO_SYSTEM_CALL_TRAIT(open)
+PITO_SYSTEM_CALL_TRAIT(openat)
+PITO_SYSTEM_CALL_TRAIT(creat)
+PITO_SYSTEM_CALL_TRAIT(fopen)
+PITO_SYSTEM_CALL_TRAIT(lchown)
+PITO_SYSTEM_CALL_TRAIT(link)
+PITO_SYSTEM_CALL_TRAIT(linkat)
+PITO_SYSTEM_CALL_TRAIT(mkdir)
+PITO_SYSTEM_CALL_TRAIT(mkdirat)
+PITO_SYSTEM_CALL_TRAIT(opendir)
+PITO_SYSTEM_CALL_TRAIT(mknod)
+PITO_SYSTEM_CALL_TRAIT(mknodat)
     // function todo: __xmknod
-    struct mkfifo {};
-    struct mkfifoat {};
-    struct access {};
-    struct faccessat {};
-    struct rename {};
-    struct renameat {};
-    struct rmdir {};
-    struct symlink {};
-    struct symlinkat {};
-    struct truncate {};
-    struct unlink {};
-    struct unlinkat {};
-    struct getcwd {};
-    struct open64 {};
-    struct openat64 {};
-    struct creat64 {};
-    struct fopen64 {};
-    struct truncate64 {};
-    struct execve {};
-    struct execv {};
-    struct execvp {};
-    struct utime {};
-    struct utimes {};
-    struct utimensat {};
-    struct futimesat {};
-    struct lutimes {};
-};
+PITO_SYSTEM_CALL_TRAIT(mkfifo)
+PITO_SYSTEM_CALL_TRAIT(mkfifoat)
+PITO_SYSTEM_CALL_TRAIT(access)
+PITO_SYSTEM_CALL_TRAIT(faccessat)
+PITO_SYSTEM_CALL_TRAIT(rename)
+PITO_SYSTEM_CALL_TRAIT(renameat)
+PITO_SYSTEM_CALL_TRAIT(rmdir)
+PITO_SYSTEM_CALL_TRAIT(symlink)
+PITO_SYSTEM_CALL_TRAIT(symlinkat)
+PITO_SYSTEM_CALL_TRAIT(truncate)
+PITO_SYSTEM_CALL_TRAIT(unlink)
+PITO_SYSTEM_CALL_TRAIT(unlinkat)
+PITO_SYSTEM_CALL_TRAIT(getcwd)
+PITO_SYSTEM_CALL_TRAIT(open64)
+PITO_SYSTEM_CALL_TRAIT(openat64)
+PITO_SYSTEM_CALL_TRAIT(creat64)
+PITO_SYSTEM_CALL_TRAIT(fopen64)
+PITO_SYSTEM_CALL_TRAIT(truncate64)
+PITO_SYSTEM_CALL_TRAIT(execve)
+PITO_SYSTEM_CALL_TRAIT(execv)
+PITO_SYSTEM_CALL_TRAIT(execvp)
+PITO_SYSTEM_CALL_TRAIT(utime)
+PITO_SYSTEM_CALL_TRAIT(utimes)
+PITO_SYSTEM_CALL_TRAIT(utimensat)
+PITO_SYSTEM_CALL_TRAIT(futimesat)
+PITO_SYSTEM_CALL_TRAIT(lutimes)
+
+}
 
 // super experts can override this!
 #ifndef PITO_SYSTEM_CALL_BASE
