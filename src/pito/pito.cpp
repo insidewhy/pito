c468d1ffd3262bb9d2b31a1a97c288c824cb1ee9
jpike <jpike@afterclap.com>
2009-01-21 18:13:10 +0000

    try to parse arguments for the pito
    git-svn-id: svn://afterclap.com/pito/trunk@61 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/pito.cpp
+++ src/pito/pito.cpp
@@ -1,4 +1,5 @@
 #include <rbutil/conf/cmd/command_line.hpp>
+#include <iostream>
 
 namespace pito {
 
@@ -10,11 +11,30 @@ bool verbose = false;
 inline int main(int argc, char *argv[]) {
     {
         using cmd_line::options_description;
+        bool showHelp = false;
 
         options_description desc;
         desc.add_options()
             ("v,verbose", verbose, "increase verbosity")
+            ("h,help", showHelp, "show help")
             ;
+
+        try {
+            cmd_line::parser(argc, argv, desc)();
+        }
+        catch (cmd_line::bad_value& e) {
+            std::cerr << "bad value reading command line options" << std::endl;
+            showHelp = true;
+        }
+        catch (cmd_line::expected_argument& e) {
+            std::cerr << "expected command line argument" << std::endl;
+            showHelp = true;
+        }
+
+        if (showHelp) {
+            // std::cout << desc << std::endl;
+            return 1;
+        }
     }
 }
 
