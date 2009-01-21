8464841b7278172907dbc53b609137aaa8be7fa0
jpike <jpike@afterclap.com>
2009-01-21 22:36:55 +0000

    more advances with pito interceptor
    git-svn-id: svn://afterclap.com/pito/trunk@69 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/config.hpp.cmake
+++ src/pito/config.hpp.cmake
@@ -2,8 +2,10 @@
 #define _PITO_CONFIG_HPP_
 
 #define _PREFIX_  "@PREFIX@"
+#define _LIB_DIR_ "@LIB_DIR@"
 #define _SHARE_DIR_ "@SHARE_DIR@"
 #define _CONFIG_DIR_ "@CONFIG_DIR@"
 #define PITO_OFF64_TYPE @OFF64_TYPE@
+#define _SHARED_LIB_FILE_EXTENSION_ "@SHARED_LIB_FILE_EXTENSION@"
 
 #endif
--- src/pito/pito.cpp
+++ src/pito/pito.cpp
@@ -1,5 +1,6 @@
 #include <rbutil/conf/cmd/command_line.hpp>
 #include <iostream>
+#include "config.hpp"
 
 namespace pito {
 
@@ -12,11 +13,13 @@ inline int main(int argc, char *argv[]) {
     {
         using cmd_line::options_description;
         bool showHelp = false;
+        std::string libraryDir;
 
         options_description desc;
         desc.add_options()
             ("v,verbose", verbose, "increase verbosity")
             ("h,help", showHelp, "show help")
+            ("l,library-dir", value(libraryDir).default_value(_LIB_DIR_), "pito library directory")
             ;
 
         try {
@@ -31,9 +34,13 @@ inline int main(int argc, char *argv[]) {
             showHelp = true;
         }
 
-        if (showHelp) {
+        if (showHelp || argv[1] == 0) {
             // std::cout << desc << std::endl;
             return 1;
+        }
+
+        if (verbose) {
+            std::cout << "load interceptor library (" << libraryDir << "libpito_" << argv[1] << _SHARED_LIB_FILE_EXTENSION_ ")" << std::endl;
         }
     }
 }
