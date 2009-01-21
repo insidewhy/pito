2d550ecf47889fc3e29057448dac774d8bafb6cb
jpike <jpike@afterclap.com>
2009-01-21 21:23:09 +0000

    exec test prints some stuff
    git-svn-id: svn://afterclap.com/pito/trunk@67 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/test/execs.c
+++ src/pito/test/execs.c
@@ -1,14 +1,19 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
     int status;
 
+    printf("fork\n");
     if (! fork()) {
         char* args[] = { "/bin/false", 0 };
+        printf("execv\n");
         execv("/bin/false", args);
     }
     wait(&status);
+    printf("system\n");
     system("/bin/false");
 
+    printf("execl\n");
     execl("/bin/ls", "-l", 0);
 }
