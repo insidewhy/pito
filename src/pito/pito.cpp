8407e537a9dafbb3a276e5fc262baaa1b3ff9d25
jpike <jpike@afterclap.com>
2009-01-21 23:36:58 +0000

    pito wrap
    git-svn-id: svn://afterclap.com/pito/trunk@70 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/pito.cpp
+++ src/pito/pito.cpp
@@ -1,6 +1,8 @@
 #include <rbutil/conf/cmd/command_line.hpp>
-#include <iostream>
 #include "config.hpp"
+
+#include <iostream>
+#include <unistd.h>
 
 namespace pito {
 
@@ -13,13 +15,15 @@ inline int main(int argc, char *argv[]) {
     {
         using cmd_line::options_description;
         bool showHelp = false;
-        std::string libraryDir;
+        std::string libraryPath;
 
+        // TODO: make all arguments from second positional and inclusive the 
+        //       new argv/argc
         options_description desc;
         desc.add_options()
             ("v,verbose", verbose, "increase verbosity")
             ("h,help", showHelp, "show help")
-            ("l,library-dir", value(libraryDir).default_value(_LIB_DIR_), "pito library directory")
+            ("l,library-dir", value(libraryPath).default_value(_LIB_DIR_), "pito library directory")
             ;
 
         try {
@@ -39,9 +43,40 @@ inline int main(int argc, char *argv[]) {
             return 1;
         }
 
-        if (verbose) {
-            std::cout << "load interceptor library (" << libraryDir << "libpito_" << argv[1] << _SHARED_LIB_FILE_EXTENSION_ ")" << std::endl;
+        if (libraryPath.empty()) {
+            std::cerr << "please do not provide an empty library directory, leave it blank to use the installed location" << std::endl;
+            return 1;
         }
+
+        std::string libraryFileName = "libpito_";
+        libraryFileName.append(argv[1]).append(_SHARED_LIB_FILE_EXTENSION_);
+
+        if ('/' != *(libraryPath.end() - 1)) libraryPath.append("/");
+        libraryPath.append(libraryFileName);
+
+        if (access(libraryPath.c_str(), R_OK)) {
+            // TODO: search for libraryFileName in LD_LIBRARY_PATH
+            std::cerr << "library " << libraryPath << " does not exist" << std::endl;
+            return 1;
+        }
+
+        // make the library an absolute path
+        if (*libraryPath.begin() != '/') {
+            char buff_[512];
+            getcwd(buff_, sizeof(buff_));
+            std::string newLibraryPath = buff_;
+            newLibraryPath.append("/").append(libraryPath);
+            libraryPath = newLibraryPath;
+        }
+
+        if (verbose) {
+            std::cout << "load interceptor library (" << libraryPath << ")" << std::endl;
+        }
+
+        // TODO: prepend it to exist library preloads
+        setenv("LD_PRELOAD", libraryPath.c_str(), 1);
+
+        execvp(argv[2], argv + 3);
     }
 }
 
