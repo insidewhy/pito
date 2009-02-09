29d496ae58c0098dafad0cca15490fc03f73b1d1
jpike <jpike@afterclap.com>
2009-02-09 03:14:53 +0000

    filesystem escape test
    git-svn-id: svn://afterclap.com/pito/trunk@163 10163329-09b4-48c1-bceb-0bacdf617a99

--- src/pito/test/break_fs_jail.c
+++ src/pito/test/break_fs_jail.c
@@ -1,11 +1,22 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    if (-1 != open("/tmp/pito-open", O_RDWR | O_CREAT, S_IWUSR))
-        return 1;
+    fprintf(stderr, "*************** filesystem jail break test ***************\n");
 
-    if (-1 != creat("/tmp/pito-creat", S_IWUSR))
-        return 1;
+    int ret = 0;
+    if (-1 != open("/tmp/pito-open", O_RDWR | O_CREAT, S_IWUSR)) {
+        fprintf(stderr, "open succeeded\n");
+        ret = 1;
+    }
+
+    if (-1 != creat("/tmp/pito-creat", S_IWUSR)) {
+        fprintf(stderr, "creat succeeded\n");
+        ret = 1;
+    }
+
+    fprintf(stderr, "\n");
+    return ret;
 }
--- src/pito/test/break_jail.c
+++ src/pito/test/break_jail.c
@@ -3,18 +3,26 @@
 
 int main(int argc, char *argv[]) {
     int status;
+    fprintf(stderr, "*************** ldpreopen jail break test ***************\n");
 
-    printf("*** fork\n");
+    fprintf(stderr, "*** fork\n");
     if (! fork()) {
         access("/tmp/cock", R_OK);
         char* args[] = { "/bin/touch", "/tmp/cock", 0 };
-        printf("*** execv\n");
+        fprintf(stderr, "*** execv\n");
         execv("/bin/touch", args);
     }
     wait(&status);
-    printf("*** system\n");
+    fprintf(stderr, "*** system\n");
     system("/bin/touch /tmp/cock");
 
-    printf("*** execl\n");
-    execl("/bin/touch", "/bin/touch", "/tmp/cock", 0);
+    fprintf(stderr, "*** execl\n");
+
+    if (! fork()) {
+        execl("/bin/touch", "/bin/touch", "/tmp/cock", 0);
+        return 0;
+    }
+    wait(&status);
+    fprintf(stderr, "\n");
+    return 0;
 }
