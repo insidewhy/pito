6d9269889c802f1b3c0f00af1cab18c1c1a77f42
jpike <jpike@afterclap.com>
2010-07-06 02:25:05 +0100

    combine break_jail with break_fs_jail

--- src/pito/test/break_fs_jail.c
+++ src/pito/test/break_fs_jail.c
@@ -8,15 +8,52 @@ int main(int argc, char *argv[]) {
 
     int ret = 0;
     if (-1 != open("/tmp/pito-open", O_RDWR | O_CREAT, S_IWUSR)) {
-        fprintf(stderr, "open succeeded\n");
+        fprintf(stderr, "!!! open succeeded :(\n");
         ret = 1;
     }
 
     if (-1 != creat("/tmp/pito-creat", S_IWUSR)) {
-        fprintf(stderr, "creat succeeded\n");
+        fprintf(stderr, "!!! creat succeeded :(\n");
         ret = 1;
     }
 
-    fprintf(stderr, "\n");
+    fprintf(stderr, "*** fork\n");
+    int status;
+    if (! fork())
+        return -1 == creat("/tmp/pito-creat", S_IWUSR);
+
+    wait(&status);
+    if (status == 0) {
+        fprintf(stderr, "!!! fork creat succeeded :(\n");
+        ret = -1;
+    }
+
+    if (! fork()) {
+        char* args[] = { "/bin/touch", "/tmp/pito-touch", 0 };
+        fprintf(stderr, "*** execv\n");
+        execv("/bin/touch", args);
+        return 0;
+    }
+    wait(&status);
+    if (status == 0) {
+        fprintf(stderr, "!!! execv touch succeeded\n");
+        ret = -1;
+    }
+
+    fprintf(stderr, "*** system\n");
+    system("/bin/touch /tmp/cock");
+
+    if (! fork()) {
+        fprintf(stderr, "*** execl\n");
+        execl("/bin/touch", "/bin/touch", "/tmp/pito-touch", 0);
+        return 0;
+    }
+    wait(&status);
+
+    if (status == 0) {
+        fprintf(stderr, "!!! execl touch succeeded :(\n");
+        ret = -1;
+    }
+
     return ret;
 }
