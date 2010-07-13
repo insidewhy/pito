ea5d6124fe34be04c327597a92ae307c07be1710
jpike <jpike@afterclap.com>
2010-07-13 22:22:37 +0100

    seperate system call handlers into their own header

--- src/pito/plugin/sandbox/libc.cpp
+++ src/pito/plugin/sandbox/libc.cpp
@@ -1,5 +1,5 @@
 #define PITO_SYSTEM_CALL_BASE  sandbox::system_call
-#include <pito/plugin/sandbox/helper.hpp>
+#include <pito/plugin/sandbox/system_call.hpp>
 #include <pito/jail/c.hpp>
 #include <pito/lib/c.hpp>
 
